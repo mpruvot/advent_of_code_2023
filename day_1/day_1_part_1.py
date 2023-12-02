cc00a22c6ade100aa36d0e3166d15b8112dab5f9
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-12-02 13:25:00 +0100

    update with check if line is none

--- day_1/day_1_part_1.py
+++ day_1/day_1_part_1.py
@@ -9,13 +9,18 @@ def get_last_digit(line: str) -> str:
         if i.isdigit():
             return i
 
-def combine_digit(line: str) -> int:
-    return int(get_first_digit(line) + get_last_digit(line))
+def combine_digits(line:str):
+    first_digit = get_first_digit(line)
+    last_digit = get_last_digit(line)
+    
+    if first_digit is None or last_digit is None:
+        return 0
+    return int(first_digit + last_digit)
 
 result = 0
 
 with open("input.txt", "r") as f:
     for i in f:
-        result += combine_digit(i)
+        result += combine_digits(i)
 
 print(result)
