daed2d17d44f57d46640c578395c161597fb5ff6
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-12-04 16:27:48 +0100

    day_4_part_1 OK

--- day_4/day_4_part_1.py
+++ day_4/day_4_part_1.py
@@ -1,31 +1,26 @@
 
 def format_string(s: str):
-    splited = s.strip().split(":")[1]
-    winning_numbers = splited.split("|")[0].strip().split(" ")
-    player_numbers = splited.split("|")[1].strip().split(" ")
-    w = [i for i in winning_numbers if i != ""]
-    p = [i for i in player_numbers if i != ""]
-        
-    return (w, p)
+    _, splited = s.strip().split(":")
+    winning_numbers = splited.split("|")[0].strip().split()
+    player_numbers = splited.split("|")[1].strip().split()      
+    return (winning_numbers, player_numbers)
 
-test_string = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
 
 def get_points(s: str):
     winning_num, player_num = format_string(s)
     result = 0
     for i in winning_num:
-        if i in player_num and i != " ":
-            if result == 0:
-                result += 1
-            else:
-                result *= 2
+        if i in player_num:
+            result = 1 if result == 0 else result * 2
     return result
 
 
-with open("input.txt", "r") as file:
-    result = 0
-    for line in file:
-        result += get_points(line)
-        
-print (result)
+def main(filepath):
+    with open(filepath, "r") as file:
+        result = 0
+        for line in file:
+            result += get_points(line)
+    return result
+            
+print(main("input.txt"))
         
