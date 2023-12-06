d39e11650dc404a3cc16e63fd573c10b30817e6c
[Marius Pruvot] <[marius.pruvot@outlook.fr]>
2023-12-06 06:27:56 +0100

    day_6

--- day_6/day_6_part_1.py
+++ day_6/day_6_part_1.py
@@ -6,14 +6,4 @@ def check_record(total_time: int, record_distance: int):
         distance = speed * travel
         if distance > record_distance:
             win += 1
-    return win  # Déplacé en dehors de la boucle
-
-times = [54, 81, 70, 88]
-distances = [446, 1292, 1035, 1007]
-
-
-wins = check_record(54817088, 446129210351007)
-  
-
-print(wins)
-
+    return win  
